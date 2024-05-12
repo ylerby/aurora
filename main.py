591696333112328d3824c1e5b96da36cf7c3a9df
aliyakbyarov.m <aliyakbyarov.m@wb.ru>
2024-05-12 19:01:04 +0300

    host, port changed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+import uvicorn
 
 app = FastAPI()
 
@@ -8,3 +9,6 @@ async def upload_photo(photo: UploadFile = File(...)):
     with open(f"photos/{photo.filename}", "wb") as f:
         f.write(photo.file.read())
     return {"filename": photo.filename}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="87.242.101.70", port=8000)
